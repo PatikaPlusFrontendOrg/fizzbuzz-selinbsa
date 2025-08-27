ef19dc04188855e968bb0e8014ca6c9017dcb8de
selinbsa <selinbasakagan@gmail.com>
2025-08-27 18:06:02 +0300

    fizzbuzz project done

--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,16 @@
 function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+  let condition = (num % 3 === 0) * 1 + (num % 5 === 0) * 2;
+
+  switch (condition) {
+    case 3:
+      return "FizzBuzz";
+    case 1:
+      return "Fizz";
+    case 2:
+      return "Buzz";
+    default:
+      return num;
+  }
 }
 
 module.exports = fizzbuzzSwitch;
